2988d8e3fc1f02d8d34543038f9dcc69ee3310ab
brandon <brandon.t.navarrete@gmail.com>
2023-03-15 10:06:18 -0500

    my main work and prepare.py

--- navs-folder/prepare.py
+++ navs-folder/prepare.py
@@ -1,9 +1,15 @@
 import pandas as pd 
 import numpy as pd 
 
-import os 
-
+import os
+import unicodedata
 import re
+import json
+
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 # ----cleaned_words------------------------
 
@@ -25,46 +31,7 @@ def create_clean_words(df, ser):
     return df
 
 # my attempt at fuzzy wuzzy 
-'''# ----fuzzy_wuzzy------------------------
 
-# Define a function to find the best match between a title and a list of options using fuzzy string matching
-# I know I normalized but just to make sure let's use the fuzzywuzzy imports to help make sure we have as many matches as possible
-
-
-def find_best_match(title, options):
-    
-    # Use the `extractOne()` function to find the best match
-    result = process.extractOne(title, options, scorer=fuzz.token_sort_ratio)
-    
-    # If a match was found, return the best match and the matching score
-    if result is not None:
-        title, best_match, score = result
-        return best_match, score
-    # Otherwise, return default values
-    else:
-        return "", 0
-
-def create_tar(df, match_series):
-    
-    # Create an empty list to store the target labels
-    target_list = []
-
-    # Iterate over each row in the df dataframe
-    for index, row in df.iterrows():
-        # Find the best match between the clean_titles and the match_series
-        best_match, score = find_best_match(row['clean_titles'], match_series)
-        # If the matching score is above a certain threshold (e.g. 80), label the row as successful
-        if score >= 50:
-            target_list.append('Successful')
-        # Otherwise, label the row as unsuccessful
-        else:
-            target_list.append('Unsuccessful')
-
-    # Add the 'Target' column to the dataframe
-    df['Target'] = target_list
-    
-    return df
-    '''
 
 # Creating target
 def creat_tar(df, ser):
@@ -80,7 +47,55 @@ def creat_tar(df, ser):
     
     return df
 
+# -----clean_text---------------
+
+def clean_article(df, col_name):
+    cleaned_summaries = []
+    for summary in df[col_name]:
+        # Normalize the summary text and convert to lowercase
+        cleaned_summary = unicodedata.normalize('NFKD', summary)\
+            .encode('ascii', 'ignore')\
+            .decode('utf-8', 'ignore')\
+            .lower()
+        cleaned_summary = re.sub(r"[^a-z0-9',\s.]", '', cleaned_summary)
+        cleaned_summaries.append(cleaned_summary)
+    df[f'cleaned_{col_name}'] = cleaned_summaries
+
     
+    
+'''nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+nltk.download('wordnet')
+'''
+
+# -----lemmatize_and_Stopwords------------------------------
+
+def lemmatize_text(text):
+    """
+    Lemmatizes input text using NLTK's WordNetLemmatizer.
+    This function first tokenizes the text, removes any non-alphabetic tokens, removes any stop words,
+    determines the part of speech of each token, and lemmatizes each token accordingly.
+    
+    Args:
+        text (str): The text to lemmatize.
+        
+    Returns:
+        str: The lemmatized text.
+    """
+    # Tokenize the text and convert to lowercase
+    tokens = word_tokenize(text.lower())
+    
+    # Remove any non-alphabetic tokens and stop words
+    tokens = [token for token in tokens if token.isalpha() and token not in stop_words]
+    
+    # Determine the part of speech of each token and lemmatize accordingly
+    pos_tags = nltk.pos_tag(tokens)
+    lemmatized_tokens = [lemmatizer.lemmatize(token, pos=pos_tag[0].lower()) if pos_tag[0].lower() in ['a', 's', 'r', 'v'] else lemmatizer.lemmatize(token) for token, pos_tag in pos_tags]
+    
+    # Join the lemmatized tokens back into a string
+    lemmatized_text = ' '.join(lemmatized_tokens)
+    return lemmatized_text
+
 
 
     
